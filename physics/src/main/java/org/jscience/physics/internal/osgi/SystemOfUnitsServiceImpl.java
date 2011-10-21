c2a20a16284a517531247656b9a92c0564232492
dautelle <dautelle@4945b2d2-9b8b-47eb-aefa-85d25e39a8f2>
2011-10-21 16:37:16 +0000

    git-svn-id: https://svn.java.net/svn/jscience~svn/trunk@60 4945b2d2-9b8b-47eb-aefa-85d25e39a8f2

---
+++ physics/src/main/java/org/jscience/physics/internal/osgi/SystemOfUnitsServiceImpl.java
@@ -0,0 +1,39 @@
+/*
+ * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
+ * Copyright (C) 2010 - JScience (http://jscience.org/)
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software is
+ * freely granted, provided that this notice is preserved.
+ */
+package org.jscience.physics.internal.osgi;
+
+import org.jscience.physics.unit.system.SI;
+import org.jscience.physics.unit.system.UCUM;
+import org.jscience.physics.unit.system.USCustomary;
+import org.unitsofmeasurement.service.SystemOfUnitsService;
+import org.unitsofmeasurement.unit.SystemOfUnits;
+
+/**
+ * SystemOfUnitsService Implementation.
+ */
+class SystemOfUnitsServiceImpl implements SystemOfUnitsService {
+
+    /**
+     * Returns the SI instance.
+     */
+    public SystemOfUnits getSystemOfUnits() {
+        return SI.getInstance();
+    }
+
+    /**
+     * Returns the instance having the specified name.
+     */
+    public SystemOfUnits getSystemOfUnits(String name) {
+        if (name.equals("SI")) return SI.getInstance();
+        if (name.equals("UCUM")) return UCUM.getInstance();
+        if (name.equals("USCustomary")) return USCustomary.getInstance();
+        return null;
+    }
+    
+}
