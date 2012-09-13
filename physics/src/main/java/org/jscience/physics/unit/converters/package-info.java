978166a47915573c5ba23f2a6c8e29344f2ac2e9
dautelle <dautelle@4945b2d2-9b8b-47eb-aefa-85d25e39a8f2>
2012-09-13 20:48:59 +0000

    Version 5.0.0
    git-svn-id: https://svn.java.net/svn/jscience~svn/trunk@680 4945b2d2-9b8b-47eb-aefa-85d25e39a8f2

--- physics/src/main/java/org/jscience/physics/unit/converters/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package provides concrete implementations of the 
- * {@link org.unitsofmeasurement.unit.UnitConverter} interface.
- */
-package org.jscience.physics.unit.converters;
--- physics/src/main/java/org/jscience/physics/unit/types/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package provides concrete implementations of the 
- * {@link org.unitsofmeasurement.unit.Unit} interface.
- */
-package org.jscience.physics.unit.types;
